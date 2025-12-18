860daba505dfa7d7bd2bad7948a7e20e5a16095c
ttnsd211 <titouan.sicard@eleves.ecmorlaix.fr>
2025-12-18 10:25:24 +0100

    Add files via upload

---
+++ couleur_aleatoire.py
@@ -0,0 +1,9 @@
+def couleur_aleatoire():
+    '''
+    Renvoie une couleur HTML valide
+    au format 'rgb(rouge, vert, bleu)'
+    où rouge, vert et bleu sont des entiers
+    compris entre 0 et 255 choisis aléatoirement.
+    
+    '''
+    return f'rgb({randint(0, 255)},{randint(0, 255)},{randint(0, 255)})'
