9d9a50d1038e855f3a6cb975e3b3014b790b73c4
Constant09 <constantin.boubennec@eleves.ecmorlaix.fr>
2026-01-08 10:20:19 +0100

    Add files via upload

---
+++ toit2.py
@@ -0,0 +1,32 @@
+
+# Dépendances
+from ma_rue import rue, affiche
+from trait import trait
+# Définitions
+
+# Fonction toit2()
+
+def toit2(x, niveau):
+    '''
+    Paramètres :
+        x : abcisse du centre du toit
+        
+        niveau : num du niveau (0 pour les rdc, ...)
+    '''
+    y = rue.height - niveau * 60 # ordonnée de la base du toit
+    
+    
+    # trait horizontal
+    rue.line_width = 10
+    rue.line_cap = "round"
+    trait(x,y,x+160,y)
+    #rue.stroke_line(x, y, x+160, y)
+
+    rue.line_width = 1
+    rue.line_cap = "butt"
+
+# Test
+if __name__ == '__main__':
+    affiche(rue)
+    for n in range(6) :
+        toit2(rue.width/2, n)
